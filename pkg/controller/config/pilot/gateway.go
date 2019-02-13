7a7db969c1de3ee3e4ebb1f175560081400fc311
Marton Sereg <martonsereg@gmail.com>
2019-02-13 09:37:54 +0100

    pilot refactored

---
+++ pkg/controller/config/pilot/gateway.go
@@ -0,0 +1,32 @@
+package pilot
+
+import (
+	istiov1beta1 "github.com/banzaicloud/istio-operator/pkg/apis/operator/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"istio.io/api/networking/v1alpha3"
+	networkingv1alpha3 "istio.io/api/pkg/kube/apis/networking/v1alpha3"
+	"github.com/banzaicloud/istio-operator/pkg/controller/config/templates"
+)
+
+var gatewaySelector = map[string]string{
+	"istio": "ingress",
+}
+
+func gateway(owner *istiov1beta1.Config) runtime.Object {
+	return &networkingv1alpha3.Gateway{
+		ObjectMeta: templates.ObjectMeta(gatewayName, nil, owner),
+		Spec: v1alpha3.Gateway{
+			Servers: []*v1alpha3.Server{
+				{
+					Port: &v1alpha3.Port{
+						Name:     "http",
+						Protocol: "HTTP2",
+						Number:   80,
+					},
+					Hosts: []string{"*"},
+				},
+			},
+			Selector: gatewaySelector,
+		},
+	}
+}
---
+++ pkg/controller/config/pilot/rbac.go
@@ -0,0 +1,86 @@
+package pilot
+
+import (
+	istiov1beta1 "github.com/banzaicloud/istio-operator/pkg/apis/operator/v1beta1"
+	"github.com/banzaicloud/istio-operator/pkg/controller/config/templates"
+	apiv1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func serviceAccount(owner *istiov1beta1.Config) runtime.Object {
+	return &apiv1.ServiceAccount{
+		ObjectMeta: templates.ObjectMeta(serviceAccountName, pilotLabels, owner),
+	}
+}
+
+func clusterRole(owner *istiov1beta1.Config) runtime.Object {
+	return &rbacv1.ClusterRole{
+		ObjectMeta: templates.ObjectMeta(clusterRoleName, pilotLabels, owner),
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{"config.istio.io"},
+				Resources: []string{"*"},
+				Verbs:     []string{"*"},
+			},
+			{
+				APIGroups: []string{"rbac.istio.io"},
+				Resources: []string{"*"},
+				Verbs:     []string{"get", "watch", "list"},
+			},
+			{
+				APIGroups: []string{"networking.istio.io"},
+				Resources: []string{"*"},
+				Verbs:     []string{"*"},
+			},
+			{
+				APIGroups: []string{"authentication.istio.io"},
+				Resources: []string{"*"},
+				Verbs:     []string{"*"},
+			},
+			{
+				APIGroups: []string{"apiextensions.k8s.io"},
+				Resources: []string{"customresourcedefinitions"},
+				Verbs:     []string{"*"},
+			},
+			{
+				APIGroups: []string{"extensions"},
+				Resources: []string{"thirdpartyresources", "thirdpartyresources.extensions", "ingresses", "ingresses/status"},
+				Verbs:     []string{"*"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"configmaps"},
+				Verbs:     []string{"create", "get", "list", "watch", "update"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"endpoints", "pods", "services"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"namespaces", "nodes", "secrets"},
+				Verbs:     []string{"get", "list", "watch"},
+			},
+		},
+	}
+}
+
+func clusterRoleBinding(owner *istiov1beta1.Config) runtime.Object {
+	return &rbacv1.ClusterRoleBinding{
+		ObjectMeta: templates.ObjectMeta(clusterRoleBindingName, pilotLabels, owner),
+		RoleRef: rbacv1.RoleRef{
+			Kind:     "ClusterRole",
+			APIGroup: "rbac.authorization.k8s.io",
+			Name:     clusterRoleName,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      serviceAccountName,
+				Namespace: owner.Namespace,
+			},
+		},
+	}
+}
