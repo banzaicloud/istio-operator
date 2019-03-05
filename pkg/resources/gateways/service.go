b52dcf63c69a428c58bb9bd154d26c8696df96a9
Zsolt Varga <zsolt.varga@console.hu>
2019-03-05 12:32:08 +0100

    use the same port name in the service as well

--- pkg/resources/gateways/service.go
+++ pkg/resources/gateways/service.go
@@ -45,7 +45,7 @@ func servicePorts(gw string) []apiv1.ServicePort {
 			{Port: 443, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(443), Name: "https", NodePort: 31390},
 			{Port: 31400, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(31400), Name: "tcp", NodePort: 31400},
 			{Port: 15029, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(15029), Name: "https-kiali", NodePort: 31410},
-			{Port: 15030, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(15030), Name: "https-prometheus", NodePort: 31420},
+			{Port: 15030, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(15030), Name: "https-prom", NodePort: 31420},
 			{Port: 15031, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(15031), Name: "https-grafana", NodePort: 31430},
 			{Port: 15032, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(15032), Name: "https-tracing", NodePort: 31440},
 			{Port: 15443, Protocol: apiv1.ProtocolTCP, TargetPort: intstr.FromInt(15443), Name: "tls", NodePort: 31450},
