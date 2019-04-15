600bf95c85d178895d5a9f6bf211e323650dc057
Zsolt Varga <zsolt.varga@console.hu>
2019-04-15 02:11:07 +0200

    fix license header

--- pkg/k8sutil/predicates.go
+++ pkg/k8sutil/predicates.go
@@ -1,5 +1,3 @@
-package k8sutil
-
 /*
 Copyright 2019 Banzai Cloud.
 
@@ -15,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+package k8sutil
 
 import (
 	"reflect"
