18cd8fcd4a6e9c9c809fe93cbd067b66ec2b7a78
Zsolt Varga <zsolt.varga@console.hu>
2019-05-29 00:39:28 +0200

    Use Caddyfile parser for Corefile manipulation

--- pkg/resources/istiocoredns/coredns.go
+++ pkg/resources/istiocoredns/coredns.go
@@ -18,11 +18,13 @@ package istiocoredns
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/goph/emperror"
+	"github.com/mholt/caddy/caddyfile"
+	"github.com/pkg/errors"
 	apiv1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
@@ -45,37 +47,47 @@ func (r *Reconciler) reconcileCoreDNSConfigMap(log logr.Logger, desiredState k8s
 		return emperror.Wrap(err, "could not get coredns configmap")
 	}
 
-	coreFile := cm.Data["CoreFile"]
-
-	var svc apiv1.Service
-	err = r.Client.Get(context.Background(), types.NamespacedName{
-		Name:      serviceName,
-		Namespace: r.Config.Namespace,
-	}, &svc)
-	if err != nil {
-		return emperror.Wrap(err, "could not get Istio coreDNS service")
-	}
-	clusterIP := []string{svc.Spec.ClusterIP}
-	global53 := fmt.Sprintf(`global:53 {
-			errors
-			cache 30
-			proxy . %s
-			}))`, clusterIP)
+	corefile := cm.Data["Corefile"]
+	desiredCorefile := corefile
+	clusterIP := ""
 
 	if desiredState == k8sutil.DesiredStatePresent {
-		if !strings.Contains(coreFile, global53) {
-			coreFile += global53
+		var svc apiv1.Service
+		err = r.Client.Get(context.Background(), types.NamespacedName{
+			Name:      serviceName,
+			Namespace: r.Config.Namespace,
+		}, &svc)
+		if err != nil {
+			return emperror.Wrap(err, "could not get Istio coreDNS service")
+		}
+		clusterIP = svc.Spec.ClusterIP
+	}
+
+	config := caddyfile.EncodedServerBlock{
+		Keys: []string{"global:53"},
+		Body: [][]interface{}{
+			{"errors"},
+			{"cache", "30"},
+			{"proxy", ".", clusterIP},
+		},
+	}
+
+	if desiredState == k8sutil.DesiredStatePresent {
+		desiredCorefile, err = r.updateCorefile([]byte(corefile), config, false)
+		if err != nil {
+			return emperror.Wrap(err, "could not add config for .global to Corefile")
 		}
 	} else if desiredState == k8sutil.DesiredStateAbsent {
-		if strings.Contains(coreFile, global53) {
-			strings.Replace(coreFile, global53, "", -1)
+		desiredCorefile, err = r.updateCorefile([]byte(corefile), config, true)
+		if err != nil {
+			return emperror.Wrap(err, "could not remove config for .global from Corefile")
 		}
 	}
 
 	if cm.Data == nil {
 		cm.Data = make(map[string]string, 0)
 	}
-	cm.Data["CoreFile"] = coreFile
+	cm.Data["Corefile"] = desiredCorefile
 
 	err = r.Client.Update(context.Background(), &cm)
 	if err != nil {
@@ -83,4 +95,55 @@ func (r *Reconciler) reconcileCoreDNSConfigMap(log logr.Logger, desiredState k8s
 	}
 
 	return nil
+}
+
+func (r *Reconciler) updateCorefile(corefile []byte, config caddyfile.EncodedServerBlock, remove bool) (string, error) {
+	corefileJSONData, err := caddyfile.ToJSON(corefile)
+	if err != nil {
+		return "", emperror.Wrap(err, "could not convert Corefile to JSON data")
+	}
+
+	var corefileJSON caddyfile.EncodedCaddyfile
+	err = json.Unmarshal(corefileJSONData, &corefileJSON)
+	if err != nil {
+		return "", emperror.Wrap(err, "could not unmarshal JSON to EncodedCaddyfile")
+	}
+
+	if len(config.Keys) < 1 {
+		return "", errors.New("invalid .global config")
+	}
+
+	pos := -1
+	for i, block := range corefileJSON {
+		if len(block.Keys) < 1 {
+			continue
+		}
+		if block.Keys[0] == config.Keys[0] {
+			pos = i
+			break
+		}
+	}
+
+	if remove && pos > 0 {
+		corefileJSON = append(corefileJSON[:pos], corefileJSON[pos+1:]...)
+	} else {
+		if pos > 0 {
+			corefileJSON[pos] = config
+		} else {
+			corefileJSON = append(corefileJSON, config)
+		}
+	}
+
+	corefileData, err := json.Marshal(&corefileJSON)
+	if err != nil {
+		return "", emperror.Wrap(err, "could not marshal EncodedCaddyfile to JSON")
+	}
+
+	corefile, err = caddyfile.FromJSON(corefileData)
+	if err != nil {
+		return "", emperror.Wrap(err, "could not convert JSON to Caddyfile")
+	}
+
+	// convert tabs to spaces for properly display content in ConfigMap
+	return strings.Replace(string(corefile), "\t", "  ", -1), nil
 }
