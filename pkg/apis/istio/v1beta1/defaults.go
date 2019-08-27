78ebfa7ebb7a0443ee8a1f792a98660672566009
László Bence Nagy <laszlonagy93@gmail.com>
2019-08-27 09:46:10 +0200

    Bump default Istio version to 1.2.5

--- pkg/apis/istio/v1beta1/defaults.go
+++ pkg/apis/istio/v1beta1/defaults.go
@@ -28,7 +28,7 @@ import (
 
 const (
 	defaultImageHub                  = "docker.io/istio"
-	defaultImageVersion              = "1.2.4"
+	defaultImageVersion              = "1.2.5"
 	defaultPilotImage                = defaultImageHub + "/" + "pilot" + ":" + defaultImageVersion
 	defaultCitadelImage              = defaultImageHub + "/" + "citadel" + ":" + defaultImageVersion
 	defaultGalleyImage               = defaultImageHub + "/" + "galley" + ":" + defaultImageVersion
