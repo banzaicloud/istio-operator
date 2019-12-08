08f055cee76842f70068ecac670ec1aa796f7145
Zsolt Varga <zsolt.varga@console.hu>
2019-12-08 22:00:30 +0100

    fix dependencies and license headers

--- pkg/crds/generate.go
+++ pkg/crds/generate.go
@@ -1,16 +1,19 @@
-// Copyright © 2019 Banzai Cloud
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2019 Banzai Cloud.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // +build ignore
 
 //go:generate go run generate.go
@@ -18,13 +21,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"path"
 	"path/filepath"
 	"runtime"
 
 	"github.com/shurcooL/vfsgen"
-	log "github.com/sirupsen/logrus"
 )
 
 var CRDs http.FileSystem = http.Dir(path.Join(getRepoRoot(), "config/base/crds"))
