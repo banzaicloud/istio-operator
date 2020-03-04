0fdea70bc2ba0fc89fd1beb464c3edb6fdd04316
László Bence Nagy <laszlonagy93@gmail.com>
2020-03-04 13:40:45 +0100

    Add/update metada exchange filters

--- pkg/resources/mixerlesstelemetry/meta_exchange_filter.go
+++ pkg/resources/mixerlesstelemetry/meta_exchange_filter.go
@@ -23,28 +23,82 @@ import (
 	"github.com/banzaicloud/istio-operator/pkg/k8sutil"
 )
 
-const metadataExchangeFilterYAML = `applyTo: HTTP_FILTER
-match:
-  context: ANY # inbound, outbound, and gateway
-  listener:
-    filterChain:
-      filter:
-        name: "envoy.http_connection_manager"
-patch:
-  operation: INSERT_BEFORE
-  value:
-    name: envoy.filters.http.wasm
-    config:
-      config:
-        configuration: envoy.wasm.metadata_exchange
-        vm_config:
-          runtime: envoy.wasm.runtime.null
-          code:
-            inline_string: envoy.wasm.metadata_exchange
+const metadataExchangeFilterYAML = `
+- applyTo: HTTP_FILTER
+  match:
+    context: ANY # inbound, outbound, and gateway
+    proxy:
+      proxyVersion: '^1\.5.*'
+    listener:
+      filterChain:
+        filter:
+          name: "envoy.http_connection_manager"
+  patch:
+    operation: INSERT_BEFORE
+    value:
+      name: envoy.filters.http.wasm
+      typed_config:
+        "@type": type.googleapis.com/udpa.type.v1.TypedStruct
+        type_url: type.googleapis.com/envoy.config.filter.http.wasm.v2.Wasm
+        value:
+          config:
+            configuration: envoy.wasm.metadata_exchange
+            vm_config:
+              runtime: envoy.wasm.runtime.null
+              code:
+                local:
+                  inline_string: envoy.wasm.metadata_exchange
+`
+
+const TCPMetadataExchangeFilterYAML = `
+    - applyTo: NETWORK_FILTER
+      match:
+        context: SIDECAR_INBOUND
+        proxy:
+          proxyVersion: '^1\.5.*'
+        listener: {}
+      patch:
+        operation: INSERT_BEFORE
+        value:
+          name: envoy.filters.network.metadata_exchange
+          config:
+            protocol: istio-peer-exchange
+    - applyTo: CLUSTER
+      match:
+        context: SIDECAR_OUTBOUND
+        proxy:
+          proxyVersion: '^1\.5.*'
+        cluster: {}
+      patch:
+        operation: MERGE
+        value:
+          filters:
+          - name: envoy.filters.network.upstream.metadata_exchange
+            typed_config:
+              "@type": type.googleapis.com/udpa.type.v1.TypedStruct
+              type_url: type.googleapis.com/envoy.tcp.metadataexchange.config.MetadataExchange
+              value:
+                protocol: istio-peer-exchange
+    - applyTo: CLUSTER
+      match:
+        context: GATEWAY
+        proxy:
+          proxyVersion: '^1\.5.*'
+        cluster: {}
+      patch:
+        operation: MERGE
+        value:
+          filters:
+          - name: envoy.filters.network.upstream.metadata_exchange
+            typed_config:
+              "@type": type.googleapis.com/udpa.type.v1.TypedStruct
+              type_url: type.googleapis.com/envoy.tcp.metadataexchange.config.MetadataExchange
+              value:
+                protocol: istio-peer-exchange
 `
 
 func (r *Reconciler) metaexchangeEnvoyFilter() *k8sutil.DynamicObject {
-	var y map[string]interface{}
+	var y []map[string]interface{}
 	yaml.Unmarshal([]byte(metadataExchangeFilterYAML), &y)
 
 	return &k8sutil.DynamicObject{
@@ -57,9 +111,27 @@ func (r *Reconciler) metaexchangeEnvoyFilter() *k8sutil.DynamicObject {
 		Name:      componentName + "-metadata-exchange",
 		Namespace: r.Config.Namespace,
 		Spec: map[string]interface{}{
-			"configPatches": []map[string]interface{}{
-				y,
-			},
+			"configPatches": y,
+		},
+		Owner: r.Config,
+	}
+}
+
+func (r *Reconciler) TCPMetaexchangeEnvoyFilter() *k8sutil.DynamicObject {
+	var y []map[string]interface{}
+	yaml.Unmarshal([]byte(TCPMetadataExchangeFilterYAML), &y)
+
+	return &k8sutil.DynamicObject{
+		Gvr: schema.GroupVersionResource{
+			Group:    "networking.istio.io",
+			Version:  "v1alpha3",
+			Resource: "envoyfilters",
+		},
+		Kind:      "EnvoyFilter",
+		Name:      componentName + "-tcp-metadata-exchange",
+		Namespace: r.Config.Namespace,
+		Spec: map[string]interface{}{
+			"configPatches": y,
 		},
 		Owner: r.Config,
 	}
