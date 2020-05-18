be72b3540cf2a9bc18019d4af2e85f5661a3d4c4
László Bence Nagy <laszlonagy93@gmail.com>
2020-05-18 09:17:10 +0200

    Bump default Istio version to 1.6.0-rc.2

--- pkg/apis/istio/v1beta1/defaults.go
+++ pkg/apis/istio/v1beta1/defaults.go
@@ -28,7 +28,7 @@ import (
 
 const (
 	defaultImageHub                   = "docker.io/istio"
-	defaultImageVersion               = "1.6.0-rc.1"
+	defaultImageVersion               = "1.6.0-rc.2"
 	defaultLogLevel                   = "default:info"
 	defaultMeshPolicy                 = PERMISSIVE
 	defaultPilotImage                 = defaultImageHub + "/" + "pilot" + ":" + defaultImageVersion
