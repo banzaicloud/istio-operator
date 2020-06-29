f66223dfe61964a37dccc53390aedf3d99290269
László Bence Nagy <laszlonagy93@gmail.com>
2020-06-29 14:54:51 +0200

    Make shellcheck happy

--- scripts/download-deps.sh
+++ scripts/download-deps.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set +x
 
-binpath=${PWD}/$(dirname $0)/../bin
+binpath=${PWD}/$(dirname "$0")/../bin
 version="v0.0.0-20180823001027-3dcf91f64f63"
 cmds="deepcopy-gen defaulter-gen lister-gen client-gen informer-gen"
 
 for name in ${cmds}; do
     if [[ ! -f $binpath/$name ]]; then
-        GOBIN=$binpath go get k8s.io/code-generator/cmd/$name@$version
+        GOBIN=$binpath go get k8s.io/code-generator/cmd/"$name"@$version
     fi
 done
 
