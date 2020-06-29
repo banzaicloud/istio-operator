046cd38f83a25ce4819df95bb229f7f0f778aaed
Zsolt Varga <zsolt.varga@console.hu>
2020-06-29 22:10:24 +0200

    cni component

--- pkg/resources/cni/configmap.go
+++ pkg/resources/cni/configmap.go
@@ -27,7 +27,7 @@ import (
 
 func (r *Reconciler) configMap() runtime.Object {
 	return &apiv1.ConfigMap{
-		ObjectMeta: templates.ObjectMeta(configMapName, nil, r.Config),
+		ObjectMeta: templates.ObjectMetaWithRevision(configMapName, nil, r.Config),
 		Data: map[string]string{
 			"cni_network_config": r.networkConfig(),
 		},
@@ -37,13 +37,15 @@ func (r *Reconciler) configMap() runtime.Object {
 func (r *Reconciler) networkConfig() string {
 	config := map[string]interface{}{
 		"cniVersion": "0.3.1",
-		"name":       "istio-cni",
+		"name":       r.Config.WithName("istio-cni"),
 		"type":       "istio-cni",
 		"log_level":  r.Config.Spec.SidecarInjector.InitCNIConfiguration.LogLevel,
 		"kubernetes": map[string]interface{}{
 			"kubeconfig":         "__KUBECONFIG_FILEPATH__",
 			"cni_bin_dir":        r.Config.Spec.SidecarInjector.InitCNIConfiguration.BinDir,
 			"exclude_namespaces": r.Config.Spec.SidecarInjector.InitCNIConfiguration.ExcludeNamespaces,
+			"include_namespaces": r.Config.Spec.SidecarInjector.InitCNIConfiguration.IncludeNamespaces,
+			"revision":           r.Config.Name,
 		},
 	}
 
