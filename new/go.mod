da5a211e37a9e3487bec69449c6a74acf75f823d
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2020-07-19 21:58:19 +0200

    Add Istio kind
    kubebuilder create api --group istio --version v1beta1 --kind Istio

--- new/go.mod
+++ new/go.mod
@@ -3,6 +3,9 @@ module github.com/banzaicloud/istio-operator
 go 1.14
 
 require (
+	github.com/go-logr/logr v0.1.0
+	github.com/onsi/ginkgo v1.11.0
+	github.com/onsi/gomega v1.8.1
 	k8s.io/apimachinery v0.17.2
 	k8s.io/client-go v0.17.2
 	k8s.io/code-generator v0.18.6 // indirect
--- new/main.go
+++ new/main.go
@@ -25,6 +25,9 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	istiov1beta1 "github.com/banzaicloud/istio-operator/api/v1beta1"
+	"github.com/banzaicloud/istio-operator/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -36,6 +39,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = istiov1beta1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +66,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.IstioReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Istio"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Istio")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
