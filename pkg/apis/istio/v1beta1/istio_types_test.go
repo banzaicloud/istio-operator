a488d82795c02ac396603a755a7f7dceda38c982
László Bence Nagy <laszlonagy93@gmail.com>
2020-08-13 09:21:22 +0200

    Bump default Istio version to 1.7.0-rc.1

--- pkg/apis/istio/v1beta1/istio_types_test.go
+++ pkg/apis/istio/v1beta1/istio_types_test.go
@@ -36,7 +36,7 @@ func TestStorageIstio(t *testing.T) {
 			Namespace: "default",
 		},
 		Spec: IstioSpec{
-			Version: "1.7.0-beta.2",
+			Version: "1.7.0-rc.1",
 		},
 	}
 	g := gomega.NewGomegaWithT(t)
