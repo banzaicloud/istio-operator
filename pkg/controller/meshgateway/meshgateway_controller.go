5922edcbd3914bf48a67c01bae4792c744935927
Zsolt Varga <zsolt.varga@console.hu>
2020-09-28 11:57:42 +0200

    fix kubebuilder rbac generation

--- pkg/controller/meshgateway/meshgateway_controller.go
+++ pkg/controller/meshgateway/meshgateway_controller.go
@@ -114,11 +114,11 @@ type ReconcileMeshGateway struct {
 	scheme  *runtime.Scheme
 }
 
-// Reconcile reads that state of the cluster for a MeshGateway object and makes changes based on the state read
-// and what is in the MeshGateway.Spec
-// Automatically generate RBAC rules to allow the Controller to read and write Deployments
 // +kubebuilder:rbac:groups=istio.banzaicloud.io,resources=meshgateways;meshgateways/finalizers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=istio.banzaicloud.io,resources=meshgateways/status,verbs=get;update;patch
+
+// Reconcile reads that state of the cluster for a MeshGateway object and makes changes based on the state read
+// and what is in the MeshGateway.Spec
 func (r *ReconcileMeshGateway) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	logger := log.WithValues("trigger", request.Namespace+"/"+request.Name, "correlationID", uuid.Must(uuid.NewV4()).String())
 
--- pkg/controller/remoteistio/remoteistio_controller.go
+++ pkg/controller/remoteistio/remoteistio_controller.go
@@ -136,13 +136,13 @@ type ReconcileRemoteConfig struct {
 	remoteClustersMgr *remoteclusters.Manager
 }
 
-// Reconcile reads that state of the cluster for a RemoteConfig object and makes changes based on the state read
-// and what is in the RemoteConfig.Spec
-// Automatically generate RBAC rules to allow the Controller to read and write Deployments
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=istio.banzaicloud.io,resources=remoteistios;remoteistios/finalizers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=istio.banzaicloud.io,resources=remoteistios/status,verbs=get;update;patch
+
+// Reconcile reads that state of the cluster for a RemoteConfig object and makes changes based on the state read
+// and what is in the RemoteConfig.Spec
 func (r *ReconcileRemoteConfig) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	logger := log.WithValues("trigger", request.Namespace+"/"+request.Name, "correlationID", uuid.Must(uuid.NewV4()).String())
 	remoteConfig := &istiov1beta1.RemoteIstio{}
