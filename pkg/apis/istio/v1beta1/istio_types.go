834143c304fd19404ce6bcb00ffb286ee422d487
Zsolt Varga <zsolt.varga@console.hu>
2020-10-29 11:31:21 +0100

    Bump default Istio version to 1.6.13

--- pkg/apis/istio/v1beta1/istio_types.go
+++ pkg/apis/istio/v1beta1/istio_types.go
@@ -35,7 +35,7 @@ const (
 )
 
 var (
-	SupportedIstioVersion = "1.6.12"
+	SupportedIstioVersion = "1.6.13"
 	Version               = "0.6.17"
 )
 
--- pkg/resources/cni/daemonset.go
+++ pkg/resources/cni/daemonset.go
@@ -227,7 +227,7 @@ func (r *Reconciler) repairImage() string {
 func (r *Reconciler) repairTag() string {
 	repairConfig := r.Config.Spec.SidecarInjector.InitCNIConfiguration.Repair
 	if util.PointerToString(repairConfig.Tag) == "" {
-		return "1.6.12"
+		return "1.6.13"
 	}
 
 	return util.PointerToString(repairConfig.Tag)
