bbbdfe4ac9280868ff09a043797d2fd41f0dcdad
Zsolt Varga <zsolt.varga@console.hu>
2020-11-18 23:16:31 +0100

    update istio crd and support both v1 and v1beta1 apiextensions versions

--- pkg/apis/addtoscheme_istio_v1beta1.go
+++ pkg/apis/addtoscheme_istio_v1beta1.go
@@ -19,6 +19,7 @@ package apis
 import (
 	securityv1beta1 "github.com/banzaicloud/istio-client-go/pkg/security/v1beta1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 
 	"github.com/banzaicloud/istio-operator/pkg/apis/istio/v1beta1"
 )
@@ -27,5 +28,6 @@ func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes, v1beta1.SchemeBuilder.AddToScheme)
 	AddToSchemes = append(AddToSchemes, apiextensionsv1.AddToScheme)
+	AddToSchemes = append(AddToSchemes, apiextensionsv1beta1.AddToScheme)
 	AddToSchemes = append(AddToSchemes, securityv1beta1.SchemeBuilder.AddToScheme)
 }
