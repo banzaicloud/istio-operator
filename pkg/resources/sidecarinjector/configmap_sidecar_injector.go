484de328c9c8f96f8d214a564684caa323d60866
Varga Zsolt <waynz0r@users.noreply.github.com>
2021-01-20 09:40:17 +0100

    Apply suggestions from code review
    fix typo
    
    Co-authored-by: Szabolcs Berecz <szabolcs.berecz@gmail.com>

--- pkg/resources/sidecarinjector/configmap_sidecar_injector.go
+++ pkg/resources/sidecarinjector/configmap_sidecar_injector.go
@@ -344,7 +344,7 @@ containers:
   - name: {{ $key }}
     value: "{{ $value }}"
 {{- end }}
-` + r.tustBundleManagerEnvVar() + `
+` + r.trustBundleManagerEnvVar() + `
 ` + r.injectedAddtionalEnvVars() + `
   imagePullPolicy: {{ .Values.global.imagePullPolicy }}
   {{ if ne (annotation .ObjectMeta ` + "`" + `status.sidecar.istio.io/port` + "`" + ` (valueOrDefault .Values.global.proxy.statusPort 0 )) ` + "`" + `0` + "`" + ` }}
@@ -609,7 +609,7 @@ func (r *Reconciler) injectedAddtionalEnvVars() string {
 	return indentWithSpaces(string(additionalEnvVars), 2)
 }
 
-func (r *Reconciler) tustBundleManagerEnvVar() string {
+func (r *Reconciler) trustBundleManagerEnvVar() string {
 	if !util.PointerToBool(r.Config.Spec.Pilot.SPIFFE.OperatorEndpoints.Enabled) {
 		return ""
 	}
