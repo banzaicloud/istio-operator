c7a4438bf9f12546cab17f515750f6cc7927c0ee
Marton Sereg <martonsereg@gmail.com>
2021-05-12 15:27:52 +0200

    scaffold meshgateway structure

---
+++ api/v1alpha1/meshgateway_types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2021 Cisco Systems, Inc. and/or its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// MeshGatewaySpec defines the desired state of MeshGateway
+type MeshGatewaySpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of MeshGateway. Edit MeshGateway_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// MeshGatewayStatus defines the observed state of MeshGateway
+type MeshGatewayStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+
+// MeshGateway is the Schema for the meshgateways API
+type MeshGateway struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MeshGatewaySpec   `json:"spec,omitempty"`
+	Status MeshGatewayStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// MeshGatewayList contains a list of MeshGateway
+type MeshGatewayList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MeshGateway `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MeshGateway{}, &MeshGatewayList{})
+}
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -68,6 +68,9 @@ var _ = BeforeSuite(func(done Done) {
 	err = servicemeshv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = servicemeshv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	// +kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
