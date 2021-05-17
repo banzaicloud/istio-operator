d29cdfb298fd339efc3d90adc3838cd3d43ef4c4
Marton Sereg <martonsereg@gmail.com>
2021-05-17 13:16:35 +0200

    meshgateway spec skaffolding

--- api/v1alpha1/meshgateway_types.go
+++ api/v1alpha1/meshgateway_types.go
@@ -20,24 +20,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// MeshGatewaySpec defines the desired state of MeshGateway
-type MeshGatewaySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of MeshGateway. Edit MeshGateway_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-}
-
-// MeshGatewayStatus defines the observed state of MeshGateway
-type MeshGatewayStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
 // +kubebuilder:object:root=true
 
 // MeshGateway is the Schema for the meshgateways API
