89ce2e7a06f7b3120c33a892a3d41a56d1c50689
Zsolt Varga <zsolt.varga@console.hu>
2021-05-25 13:11:59 +0200

    bump version

--- pkg/apis/istio/v1beta1/defaults.go
+++ pkg/apis/istio/v1beta1/defaults.go
@@ -28,9 +28,9 @@ import (
 
 const (
 	banzaiImageHub                         = "ghcr.io/banzaicloud"
-	banzaiImageVersion                     = "1.9.5-bzc"
+	banzaiImageVersion                     = "1.10.0-bzc-rc.0"
 	defaultImageHub                        = "gcr.io/istio-release"
-	defaultImageVersion                    = "1.9.5"
+	defaultImageVersion                    = "1.10.0"
 	defaultLogLevel                        = "default:info"
 	defaultMeshPolicy                      = PERMISSIVE
 	defaultPilotImage                      = defaultImageHub + "/" + "pilot" + ":" + defaultImageVersion
