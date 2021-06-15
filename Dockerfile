4175074faace908f780a1533cc7e0ad02a3b797a
Laszlo Bence Nagy <laszlonagy93@gmail.com>
2021-06-15 17:55:44 +0200

    Move static file generation to have its own go.mod

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ COPY go.sum go.sum
 # Copy the API Go Modules manifests
 COPY api/go.mod api/go.mod
 COPY api/go.sum api/go.sum
+# Copy the static Go Modules manifests
+COPY static/go.mod static/go.mod
+COPY static/go.sum static/go.sum
 # cache deps before building and copying source so that we don't need to re-download as much
 # and so that source changes don't invalidate our downloaded layer
 RUN go mod download
@@ -17,6 +20,7 @@ COPY main.go main.go
 COPY api/ api/
 COPY controllers/ controllers/
 COPY internal/ internal/
+COPY static/ static/
 
 # Build
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o manager main.go
---
+++ static/util/zeromodfs.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 Cisco Systems, Inc. and/or its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"net/http"
+	"os"
+	"time"
+)
+
+// ZeroModTimeFileSystem is an http.FileSystem wrapper.
+// It exposes a filesystem exactly like Source, except
+// all file modification times are changed to zero.
+type ZeroModTimeFileSystem struct {
+	Source http.FileSystem
+}
+
+func (fs ZeroModTimeFileSystem) Open(name string) (http.File, error) {
+	f, err := fs.Source.Open(name)
+
+	return file{f}, err
+}
+
+type file struct {
+	http.File
+}
+
+func (f file) Stat() (os.FileInfo, error) {
+	fi, err := f.File.Stat()
+
+	return fileInfo{fi}, err
+}
+
+type fileInfo struct {
+	os.FileInfo
+}
+
+func (fi fileInfo) ModTime() time.Time { return time.Time{} }
