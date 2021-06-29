0f5021d3ec83bab5425abe51848d5443d44e7d87
Zsolt Varga <zsolt.varga@console.hu>
2021-06-29 13:39:24 +0200

    fix cue-gen download

--- scripts/download-deps.sh
+++ scripts/download-deps.sh
@@ -26,6 +26,9 @@ function ensure-binary-version() {
         BUILD_DIR=$(mktemp -d)
         pushd "${BUILD_DIR}"
         go mod init foobar
+        if [ ${bin_name} == "cue-gen" ]; then
+            go mod edit -replace cuelang.org/go=github.com/waynz0r/cue@v0.2.3-0.20210629110240-49370731dc3c
+        fi
         GOBIN=${PWD} go get "${download_location}"
         mkdir -p "${binpath}"
         mv "${bin_name}" "${binpath}/${target_name}"
