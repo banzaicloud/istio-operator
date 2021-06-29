453f838c01a6217579d995575c08bfed9efef1cf
Laszlo Bence Nagy <laszlonagy93@gmail.com>
2021-06-29 14:36:35 +0200

    Add logging.level to ICP spec

--- api/v1alpha1/istiocontrolplane.pb.go
+++ api/v1alpha1/istiocontrolplane.pb.go
@@ -50,8 +50,10 @@ type IstioControlPlaneSpec struct {
 	// Contains the intended version for the Istio control plane.
 	// +kubebuilder:validation:Pattern=^1.
 	Version string `protobuf:"bytes,1,opt,name=version,proto3" json:"version"`
+	// Logging configurations.
+	Logging *LoggingConfiguration `protobuf:"bytes,2,opt,name=logging,proto3" json:"logging"`
 	// Defines mesh-wide settings for the Istio control plane.
-	MeshConfig           *v1alpha1.MeshConfig `protobuf:"bytes,2,opt,name=mesh_config,json=meshConfig,proto3" json:"mesh_config,omitempty"`
+	MeshConfig           *v1alpha1.MeshConfig `protobuf:"bytes,3,opt,name=mesh_config,json=meshConfig,proto3" json:"mesh_config,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
 	XXX_unrecognized     []byte               `json:"-"`
 	XXX_sizecache        int32                `json:"-"`
@@ -97,11 +99,69 @@ func (m *IstioControlPlaneSpec) GetVersion() string {
 	return ""
 }
 
+func (m *IstioControlPlaneSpec) GetLogging() *LoggingConfiguration {
+	if m != nil {
+		return m.Logging
+	}
+	return nil
+}
+
 func (m *IstioControlPlaneSpec) GetMeshConfig() *v1alpha1.MeshConfig {
 	if m != nil {
 		return m.MeshConfig
 	}
 	return nil
+}
+
+// Comma-separated minimum per-scope logging level of messages to output, in the form of <scope>:<level>,<scope>:<level>
+// The control plane has different scopes depending on component, but can configure default log level across all components
+// If empty, default scope and level will be used as configured in code
+type LoggingConfiguration struct {
+	// +kubebuilder:validation:Pattern=`^([a-zA-Z]+:[a-zA-Z]+,?)+$`
+	Level                string   `protobuf:"bytes,1,opt,name=level,proto3" json:"level"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *LoggingConfiguration) Reset()         { *m = LoggingConfiguration{} }
+func (m *LoggingConfiguration) String() string { return proto.CompactTextString(m) }
+func (*LoggingConfiguration) ProtoMessage()    {}
+func (*LoggingConfiguration) Descriptor() ([]byte, []int) {
+	return fileDescriptor_6817de833805cb8b, []int{1}
+}
+func (m *LoggingConfiguration) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *LoggingConfiguration) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_LoggingConfiguration.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *LoggingConfiguration) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LoggingConfiguration.Merge(m, src)
+}
+func (m *LoggingConfiguration) XXX_Size() int {
+	return m.Size()
+}
+func (m *LoggingConfiguration) XXX_DiscardUnknown() {
+	xxx_messageInfo_LoggingConfiguration.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LoggingConfiguration proto.InternalMessageInfo
+
+func (m *LoggingConfiguration) GetLevel() string {
+	if m != nil {
+		return m.Level
+	}
+	return ""
 }
 
 // <!-- go code generation tags
@@ -118,7 +178,7 @@ func (m *IstioControlPlaneStatus) Reset()         { *m = IstioControlPlaneStatus
 func (m *IstioControlPlaneStatus) String() string { return proto.CompactTextString(m) }
 func (*IstioControlPlaneStatus) ProtoMessage()    {}
 func (*IstioControlPlaneStatus) Descriptor() ([]byte, []int) {
-	return fileDescriptor_6817de833805cb8b, []int{1}
+	return fileDescriptor_6817de833805cb8b, []int{2}
 }
 func (m *IstioControlPlaneStatus) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -149,6 +209,7 @@ var xxx_messageInfo_IstioControlPlaneStatus proto.InternalMessageInfo
 
 func init() {
 	proto.RegisterType((*IstioControlPlaneSpec)(nil), "istio_operator.v2.api.v1alpha1.IstioControlPlaneSpec")
+	proto.RegisterType((*LoggingConfiguration)(nil), "istio_operator.v2.api.v1alpha1.LoggingConfiguration")
 	proto.RegisterType((*IstioControlPlaneStatus)(nil), "istio_operator.v2.api.v1alpha1.IstioControlPlaneStatus")
 }
 
@@ -157,25 +218,29 @@ func init() {
 }
 
 var fileDescriptor_6817de833805cb8b = []byte{
-	// 287 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x90, 0x3f, 0x4b, 0xc4, 0x30,
-	0x18, 0xc6, 0xc9, 0x0d, 0x8a, 0xb9, 0x41, 0x28, 0x8a, 0x67, 0x87, 0xf6, 0x38, 0x1c, 0xce, 0xc1,
-	0x84, 0xab, 0xb8, 0xeb, 0x75, 0x72, 0x10, 0xe4, 0xdc, 0x5c, 0x4a, 0xda, 0xcb, 0xa5, 0x81, 0xb4,
-	0x6f, 0x48, 0xd3, 0x0e, 0xce, 0x7e, 0x38, 0x47, 0x3f, 0x81, 0x48, 0x47, 0x3f, 0x85, 0xa4, 0xb1,
-	0xa7, 0xe0, 0xf6, 0xf0, 0xfe, 0xfd, 0x3d, 0x0f, 0xbe, 0x60, 0x5a, 0xd2, 0x6e, 0xc5, 0x94, 0x2e,
-	0xd9, 0x8a, 0xca, 0xc6, 0x4a, 0x28, 0xa0, 0xb6, 0x06, 0x94, 0x56, 0xac, 0xe6, 0x44, 0x1b, 0xb0,
-	0x10, 0x44, 0x43, 0x23, 0x03, 0xcd, 0x0d, 0xb3, 0x60, 0x48, 0x97, 0x10, 0xa6, 0x25, 0x19, 0xf7,
-	0xc2, 0xb0, 0xe2, 0x4d, 0xf9, 0x7b, 0xa6, 0x80, 0x7a, 0x27, 0x85, 0xdf, 0x0d, 0x4f, 0x04, 0x08,
-	0x18, 0x24, 0x75, 0xea, 0xa7, 0x1a, 0x0b, 0x00, 0xa1, 0x38, 0x75, 0xef, 0x77, 0x92, 0xab, 0x6d,
-	0x96, 0xf3, 0x92, 0x75, 0x12, 0x8c, 0x1f, 0x58, 0xbc, 0x22, 0x7c, 0x7a, 0xef, 0xbe, 0xa6, 0x1e,
-	0xe7, 0xd1, 0xe1, 0x3c, 0x69, 0x5e, 0x04, 0x97, 0xf8, 0xb0, 0xe3, 0xa6, 0x91, 0x50, 0xcf, 0xd0,
-	0x1c, 0x2d, 0x8f, 0xd6, 0xc7, 0xfd, 0x1d, 0x9a, 0x7c, 0x7d, 0xc4, 0x63, 0x79, 0x33, 0x8a, 0xe0,
-	0x16, 0x4f, 0x1d, 0x59, 0xe6, 0x81, 0x66, 0x93, 0x39, 0x5a, 0x4e, 0x93, 0x98, 0x0c, 0x6e, 0x88,
-	0xeb, 0xec, 0x2d, 0x90, 0x07, 0xde, 0x94, 0xe9, 0x30, 0xb6, 0xc1, 0xd5, 0x5e, 0x2f, 0xce, 0xf1,
-	0xd9, 0x7f, 0x0a, 0xcb, 0x6c, 0xdb, 0xac, 0xd3, 0xb7, 0x3e, 0x42, 0xef, 0x7d, 0x84, 0x3e, 0xfb,
-	0x08, 0x3d, 0xdf, 0x08, 0x69, 0xcb, 0x36, 0x27, 0x05, 0x54, 0x34, 0x67, 0xf5, 0x0b, 0x93, 0x85,
-	0x82, 0x76, 0xeb, 0x23, 0xbd, 0x1a, 0x93, 0xa3, 0x5d, 0x42, 0xff, 0x26, 0x9e, 0x1f, 0x0c, 0x6e,
-	0xaf, 0xbf, 0x03, 0x00, 0x00, 0xff, 0xff, 0x3a, 0xae, 0xc3, 0xad, 0x88, 0x01, 0x00, 0x00,
+	// 348 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x52, 0x3d, 0x4f, 0xeb, 0x30,
+	0x14, 0x95, 0xfb, 0xf4, 0x5e, 0x55, 0x77, 0x78, 0x52, 0x54, 0x44, 0xe9, 0x90, 0x54, 0x15, 0x43,
+	0x19, 0x70, 0xd4, 0x02, 0x62, 0x85, 0x76, 0x42, 0x02, 0x09, 0x85, 0x0d, 0x86, 0xca, 0x49, 0x5d,
+	0xc7, 0x92, 0x9b, 0x6b, 0x39, 0x4e, 0x06, 0x7e, 0x21, 0x23, 0xbf, 0xa0, 0x82, 0x8c, 0xfd, 0x15,
+	0xc8, 0x71, 0x43, 0x91, 0x40, 0x4c, 0x3e, 0xba, 0x1f, 0xe7, 0xdc, 0x73, 0xaf, 0xf1, 0x31, 0x55,
+	0x22, 0x2c, 0x27, 0x54, 0xaa, 0x94, 0x4e, 0x42, 0x91, 0x1b, 0x01, 0x09, 0x64, 0x46, 0x83, 0x54,
+	0x92, 0x66, 0x8c, 0x28, 0x0d, 0x06, 0x3c, 0xbf, 0x4e, 0x2c, 0x40, 0x31, 0x4d, 0x0d, 0x68, 0x52,
+	0x4e, 0x09, 0x55, 0x82, 0x34, 0x7d, 0x83, 0xc1, 0x9a, 0xe5, 0xe9, 0x9e, 0x26, 0x81, 0x6c, 0x25,
+	0xb8, 0xeb, 0x1d, 0xf4, 0x38, 0x70, 0xa8, 0x61, 0x68, 0xd1, 0x2e, 0x1a, 0x70, 0x00, 0x2e, 0x59,
+	0x68, 0xe5, 0x57, 0x82, 0xc9, 0xe5, 0x22, 0x66, 0x29, 0x2d, 0x05, 0x68, 0x57, 0x30, 0x7a, 0x47,
+	0xf8, 0xe0, 0xc6, 0xaa, 0xce, 0xdd, 0x38, 0xf7, 0x76, 0x9c, 0x07, 0xc5, 0x12, 0xef, 0x04, 0xb7,
+	0x4b, 0xa6, 0x73, 0x01, 0x59, 0x1f, 0x0d, 0xd1, 0xb8, 0x33, 0xfb, 0x5f, 0x5d, 0xa3, 0xd6, 0x76,
+	0x13, 0x34, 0xe1, 0xa8, 0x01, 0xde, 0x13, 0x6e, 0x4b, 0xe0, 0x5c, 0x64, 0xbc, 0xdf, 0x1a, 0xa2,
+	0x71, 0x77, 0x7a, 0x4e, 0x7e, 0x77, 0x42, 0x6e, 0x5d, 0xf9, 0xbc, 0x76, 0x50, 0x68, 0x6a, 0x04,
+	0x64, 0xb3, 0xae, 0x25, 0xdf, 0x11, 0x45, 0x0d, 0xf0, 0xae, 0x70, 0xd7, 0xda, 0x5e, 0x38, 0xb7,
+	0xfd, 0x3f, 0xb5, 0x40, 0xe0, 0x04, 0x88, 0xcd, 0xec, 0x59, 0xef, 0x58, 0x9e, 0x3a, 0xca, 0x08,
+	0xaf, 0x3f, 0xf1, 0xe8, 0x12, 0xf7, 0x7e, 0xd2, 0xf3, 0x02, 0xfc, 0x57, 0xb2, 0x92, 0xc9, 0x9d,
+	0xbf, 0xce, 0x76, 0x13, 0xb8, 0x40, 0xe4, 0x9e, 0xd1, 0x11, 0x3e, 0xfc, 0xbe, 0x1b, 0x43, 0x4d,
+	0x91, 0xcf, 0xe6, 0x2f, 0x95, 0x8f, 0x5e, 0x2b, 0x1f, 0xbd, 0x55, 0x3e, 0x7a, 0xbc, 0xe0, 0xc2,
+	0xa4, 0x45, 0x4c, 0x12, 0x58, 0x87, 0x31, 0xcd, 0x9e, 0xa9, 0x48, 0x24, 0x14, 0x4b, 0x77, 0xe8,
+	0xd3, 0x66, 0x0b, 0x61, 0x39, 0x0d, 0xbf, 0xfe, 0x83, 0xf8, 0x5f, 0x7d, 0x83, 0xb3, 0x8f, 0x00,
+	0x00, 0x00, 0xff, 0xff, 0x97, 0xa5, 0x39, 0xa5, 0x1e, 0x02, 0x00, 0x00,
 }
 
 func (m *IstioControlPlaneSpec) Marshal() (dAtA []byte, err error) {
@@ -212,12 +277,58 @@ func (m *IstioControlPlaneSpec) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 			i = encodeVarintIstiocontrolplane(dAtA, i, uint64(size))
 		}
 		i--
+		dAtA[i] = 0x1a
+	}
+	if m.Logging != nil {
+		{
+			size, err := m.Logging.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintIstiocontrolplane(dAtA, i, uint64(size))
+		}
+		i--
 		dAtA[i] = 0x12
 	}
 	if len(m.Version) > 0 {
 		i -= len(m.Version)
 		copy(dAtA[i:], m.Version)
 		i = encodeVarintIstiocontrolplane(dAtA, i, uint64(len(m.Version)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *LoggingConfiguration) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *LoggingConfiguration) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *LoggingConfiguration) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if len(m.Level) > 0 {
+		i -= len(m.Level)
+		copy(dAtA[i:], m.Level)
+		i = encodeVarintIstiocontrolplane(dAtA, i, uint64(len(m.Level)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -272,8 +383,28 @@ func (m *IstioControlPlaneSpec) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovIstiocontrolplane(uint64(l))
 	}
+	if m.Logging != nil {
+		l = m.Logging.Size()
+		n += 1 + l + sovIstiocontrolplane(uint64(l))
+	}
 	if m.MeshConfig != nil {
 		l = m.MeshConfig.Size()
+		n += 1 + l + sovIstiocontrolplane(uint64(l))
+	}
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *LoggingConfiguration) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Level)
+	if l > 0 {
 		n += 1 + l + sovIstiocontrolplane(uint64(l))
 	}
 	if m.XXX_unrecognized != nil {
@@ -363,6 +494,42 @@ func (m *IstioControlPlaneSpec) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Logging", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowIstiocontrolplane
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthIstiocontrolplane
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthIstiocontrolplane
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Logging == nil {
+				m.Logging = &LoggingConfiguration{}
+			}
+			if err := m.Logging.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field MeshConfig", wireType)
 			}
 			var msglen int
@@ -396,6 +563,89 @@ func (m *IstioControlPlaneSpec) Unmarshal(dAtA []byte) error {
 			if err := m.MeshConfig.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipIstiocontrolplane(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthIstiocontrolplane
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *LoggingConfiguration) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowIstiocontrolplane
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: LoggingConfiguration: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: LoggingConfiguration: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Level", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowIstiocontrolplane
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthIstiocontrolplane
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthIstiocontrolplane
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Level = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
--- api/v1alpha1/istiocontrolplane_json.gen.go
+++ api/v1alpha1/istiocontrolplane_json.gen.go
@@ -30,6 +30,17 @@ func (this *IstioControlPlaneSpec) UnmarshalJSON(b []byte) error {
 	return IstiocontrolplaneUnmarshaler.Unmarshal(bytes.NewReader(b), this)
 }
 
+// MarshalJSON is a custom marshaler for LoggingConfiguration
+func (this *LoggingConfiguration) MarshalJSON() ([]byte, error) {
+	str, err := IstiocontrolplaneMarshaler.MarshalToString(this)
+	return []byte(str), err
+}
+
+// UnmarshalJSON is a custom unmarshaler for LoggingConfiguration
+func (this *LoggingConfiguration) UnmarshalJSON(b []byte) error {
+	return IstiocontrolplaneUnmarshaler.Unmarshal(bytes.NewReader(b), this)
+}
+
 // MarshalJSON is a custom marshaler for IstioControlPlaneStatus
 func (this *IstioControlPlaneStatus) MarshalJSON() ([]byte, error) {
 	str, err := IstiocontrolplaneMarshaler.MarshalToString(this)
