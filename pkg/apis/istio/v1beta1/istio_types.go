2b16972b23da4bd4d09534fcf82a157218713674
Laszlo Bence Nagy <laszlonagy93@gmail.com>
2021-07-09 11:24:09 +0200

    Bump image and chart versions

--- pkg/apis/istio/v1beta1/istio_types.go
+++ pkg/apis/istio/v1beta1/istio_types.go
@@ -43,7 +43,7 @@ const (
 
 var (
 	SupportedIstioVersion = "1.10.2"
-	Version               = "0.10.0"
+	Version               = "0.10.1"
 
 	DisableInjectionLabel = map[string]string{
 		ManualInjectionLabelKey: "false",
