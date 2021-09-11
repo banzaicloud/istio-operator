9fb0f9aa8cb82bacbbfea0d9c15b957a8e91e1c7
Zsolt Varga <zsolt.varga@console.hu>
2021-09-11 23:18:19 +0200

    add sidecar injector reconciler with tests

---
+++ internal/components/sidecarinjector/reconcile.go
@@ -0,0 +1,102 @@
+/*
+Copyright 2021 Cisco Systems, Inc. and/or its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sidecarinjector
+
+import (
+	"net/http"
+
+	"emperror.dev/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/banzaicloud/istio-operator/v2/api/v1alpha1"
+	assets "github.com/banzaicloud/istio-operator/v2/internal/assets"
+	"github.com/banzaicloud/istio-operator/v2/internal/components"
+	"github.com/banzaicloud/istio-operator/v2/internal/util"
+	"github.com/banzaicloud/operator-tools/pkg/helm"
+	"github.com/banzaicloud/operator-tools/pkg/helm/templatereconciler"
+)
+
+const (
+	componentName = "istio-sidecar-injector"
+	chartName     = "istio-sidecar-injector"
+	releaseName   = "istio-sidecar-injector"
+
+	valuesTemplateFileName = "values.yaml.tpl"
+)
+
+var _ components.MinimalComponent = &Component{}
+
+type Component struct{}
+
+func NewChartReconciler(helmReconciler *templatereconciler.HelmReconciler) components.ComponentReconciler {
+	return &components.Base{
+		HelmReconciler: helmReconciler,
+		Component:      &Component{},
+	}
+}
+
+func (rec *Component) Name() string {
+	return componentName
+}
+
+func (rec *Component) Enabled(object runtime.Object) bool {
+	if controlPlane, ok := object.(*v1alpha1.IstioControlPlane); ok {
+		return controlPlane.DeletionTimestamp.IsZero() && controlPlane.GetSpec().GetMode() == v1alpha1.ModeType_PASSIVE
+	}
+
+	return true
+}
+
+func (rec *Component) ReleaseData(object runtime.Object) (*templatereconciler.ReleaseData, error) {
+	icp, ok := object.(*v1alpha1.IstioControlPlane)
+	if !ok {
+		return nil, errors.WrapIff(errors.NewPlain("object cannot be converted to an IstioControlPlane"), "%+v", object)
+	}
+
+	values, err := rec.values(object)
+	if err != nil {
+		return nil, errors.WithStackIf(err)
+	}
+
+	overlays, err := util.ConvertK8sOverlays(icp.GetSpec().GetK8SResourceOverlays())
+	if err != nil {
+		return nil, errors.WrapIf(err, "could not convert k8s resource overlays")
+	}
+
+	return &templatereconciler.ReleaseData{
+		Chart:       http.FS(assets.IstioSidecarInjector),
+		Values:      values,
+		Namespace:   icp.Namespace,
+		ChartName:   chartName,
+		ReleaseName: releaseName,
+		Layers:      overlays,
+	}, nil
+}
+
+func (rec *Component) values(object runtime.Object) (helm.Strimap, error) {
+	icp, ok := object.(*v1alpha1.IstioControlPlane)
+	if !ok {
+		return nil, errors.WrapIff(errors.NewPlain("object cannot be converted to an IstioControlPlane"), "%+v", object)
+	}
+
+	values, err := util.TransformStructToStriMapWithTemplate(icp, assets.IstioSidecarInjector, valuesTemplateFileName)
+	if err != nil {
+		return nil, errors.WrapIff(err, "IstioControlPlane spec cannot be converted into a map[string]interface{}: %+v", icp.Spec)
+	}
+
+	return values, nil
+}
