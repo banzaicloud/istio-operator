c0f8497556b746482794fb5301c33c69180c7652
Zsolt Varga <zsolt.varga@console.hu>
2021-10-11 20:16:49 +0200

    add resource requirements convert helpers

--- api/v1alpha1/istiocontrolplane_types.go
+++ api/v1alpha1/istiocontrolplane_types.go
@@ -21,6 +21,7 @@ import (
 	"strings"
 
 	v1alpha1 "istio.io/api/mesh/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -170,6 +171,34 @@ func (icp *PeerIstioControlPlane) GetSpec() *IstioControlPlaneSpec {
 	}
 
 	return nil
+}
+
+func (r *ResourceRequirements) ConvertToK8sRR() *corev1.ResourceRequirements {
+	rr := &corev1.ResourceRequirements{
+		Limits:   make(corev1.ResourceList),
+		Requests: make(corev1.ResourceList),
+	}
+
+	for k, v := range r.Limits {
+		rr.Limits[corev1.ResourceName(k)] = v.Quantity
+	}
+
+	return rr
+}
+
+func InitResourceRequirementsFromK8sRR(rr *corev1.ResourceRequirements) *ResourceRequirements {
+	r := &ResourceRequirements{
+		Limits:   make(map[string]*Quantity),
+		Requests: make(map[string]*Quantity),
+	}
+
+	for k, v := range rr.Limits {
+		r.Limits[string(k)] = &Quantity{
+			Quantity: v,
+		}
+	}
+
+	return r
 }
 
 // PeerIstioControlPlaneList contains a list of PeerIstioControlPlane
