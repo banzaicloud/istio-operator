5b380e10c247a344cf3fa4d4f1673f409d6d2e3e
Kristof Gyuracz <kristof.gyuracz@gmail.com>
2022-05-12 10:53:53 +0200

    various fixes

--- cmd/docs.go
+++ cmd/docs.go
@@ -1,16 +1,18 @@
-// Copyright © 2022 Banzai Cloud
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright 2022 Cisco Systems, Inc. and/or its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package main
 
@@ -20,8 +22,9 @@ import (
 
 	"emperror.dev/errors"
 	"github.com/MakeNowJust/heredoc"
-	"github.com/banzaicloud/operator-tools/pkg/docgen"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/banzaicloud/operator-tools/pkg/docgen"
 )
 
 var logger = zap.New(zap.UseDevMode(true))
@@ -78,6 +81,7 @@ func crds() {
 			filepath.Join(relPath, document.Item.Name),
 			document.Desc,
 			document.Item.Category))
+
 		return nil
 	}
 
