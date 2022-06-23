365a666f77e03e7369439916e1cd3e6e9e95dd01
Kristof Gyuracz <kristof.gyuracz@gmail.com>
2022-06-23 13:45:03 +0200

    initial support of 1.13.5

--- internal/components/base/base_test.go
+++ internal/components/base/base_test.go
@@ -59,7 +59,7 @@ func TestICPBaseResourceDump(t *testing.T) {
 			reconciler.NativeReconcilerSetControllerRef(),
 		}),
 		logger.NewWithLogrLogger(logr.NewTestLogger(t)),
-		"1.13.3",
+		"1.13.5",
 	)
 
 	dd, err := reconciler.GetManifest(icp)
