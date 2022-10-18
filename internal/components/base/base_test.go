04a22c74a699482b80740a3c4aaf2a4225b1ad8e
LuciferInLove <lucifer.in.love@protonmail.com>
2022-10-18 14:15:10 +0200

    bump istio version to 1.15.2

--- internal/components/base/base_test.go
+++ internal/components/base/base_test.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -59,7 +59,7 @@ func TestICPBaseResourceDump(t *testing.T) {
 			reconciler.NativeReconcilerSetControllerRef(),
 		}),
 		logger.NewWithLogrLogger(logr.NewTestLogger(t)),
-		"1.15.0",
+		"1.15.2",
 	)
 
 	dd, err := reconciler.GetManifest(icp)
