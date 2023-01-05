db6e5a01eb6e34ab1a0bce342aca899efba7078d
Kristof Gyuracz <kristof.gyuracz@gmail.com>
2023-01-05 10:43:18 +0100

    enable multiple custom templates

--- api/v1alpha1/istiocontrolplane_json.gen.go
+++ api/v1alpha1/istiocontrolplane_json.gen.go
@@ -28,14 +28,25 @@ func (this *SidecarInjectorConfiguration) UnmarshalJSON(b []byte) error {
 	return IstiocontrolplaneUnmarshaler.Unmarshal(bytes.NewReader(b), this)
 }
 
-// MarshalJSON is a custom marshaler for Templates
-func (this *Templates) MarshalJSON() ([]byte, error) {
+// MarshalJSON is a custom marshaler for SidecarInjectionTemplates
+func (this *SidecarInjectionTemplates) MarshalJSON() ([]byte, error) {
 	str, err := IstiocontrolplaneMarshaler.MarshalToString(this)
 	return []byte(str), err
 }
 
-// UnmarshalJSON is a custom unmarshaler for Templates
-func (this *Templates) UnmarshalJSON(b []byte) error {
+// UnmarshalJSON is a custom unmarshaler for SidecarInjectionTemplates
+func (this *SidecarInjectionTemplates) UnmarshalJSON(b []byte) error {
+	return IstiocontrolplaneUnmarshaler.Unmarshal(bytes.NewReader(b), this)
+}
+
+// MarshalJSON is a custom marshaler for CustomSidecarInjectionTemplates
+func (this *CustomSidecarInjectionTemplates) MarshalJSON() ([]byte, error) {
+	str, err := IstiocontrolplaneMarshaler.MarshalToString(this)
+	return []byte(str), err
+}
+
+// UnmarshalJSON is a custom unmarshaler for CustomSidecarInjectionTemplates
+func (this *CustomSidecarInjectionTemplates) UnmarshalJSON(b []byte) error {
 	return IstiocontrolplaneUnmarshaler.Unmarshal(bytes.NewReader(b), this)
 }
 
